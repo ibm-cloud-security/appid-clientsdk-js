fdcb69c3a8c09b473cb3415a4462f732cf5c17f1
Joyce Huang <huangjoyce3@gmail.com>
2019-08-14 10:13:05 -0500

    update samples folder

--- sample/server.js
+++ sample/server.js
@@ -1,18 +1,11 @@
 const express = require('express');
-const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 3005;
 
 // Set public folder as root
 app.use(express.static("../dist"));
 app.use(express.static("public"));
-
-// Allow front-end access to node_modules folder
-// app.use('/AppIDClient', express.static(`../dist/AppIDClient`));
-
-var oidc = require('./oidc.js');
-let url = 'http://localhost:3005';
-oidc(url, app);
 
 // Listen for HTTP requests on port 3000
 app.listen(port, () => {
