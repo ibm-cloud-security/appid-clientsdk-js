62bc146be4109a4081ea4200045960c1e82ac7c2
Bernie <bernhardtramat@gmail.com>
2019-08-27 21:57:32 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import {
   MatInputModule,
   MatCardModule,
@@ -11,33 +10,25 @@ import {
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
-import { PostListComponent } from './posts/post-list/post-list.component';
-import { HttpClientModule } from '@angular/common/http';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent,
     HeaderComponent,
-    PostListComponent,
     WelcomeComponent
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
-    MatExpansionModule,
-    HttpClientModule
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+declare const AppID: any;
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
-export class WelcomeComponent implements OnInit {
+export class WelcomeComponent {
+  show = false;
+  userName = 'To get started Login or Sign up below';
+  idToken = '';
+  userData = '';
+  buttonDisplay = 'show';
+  credentials = 'hide';
 
-  constructor() { }
-
-  ngOnInit() {
+  async onLoginClick() {
+    const appid = new AppID();
+    await appid.init({
+      clientId: '<SPA_CLIENT_ID>',
+      discoveryEndpoint: '<WELL_KNOWN_ENDPOINT>'
+    });
+    const tokens = await appid.signinWithPopup();
+    const userInfo =  await appid.getUserInfo(tokens.accessToken);
+    const decodeTokens = tokens.idTokenPayload;
+    this.userName = 'Welcome ' + decodeTokens.name;
+    this.buttonDisplay = 'hide';
+    this.idToken = JSON.stringify(decodeTokens);
+    this.userData = JSON.stringify(userInfo);
+    this.credentials = 'show';
   }
-
 }
