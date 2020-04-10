7ba0b00218307b59bb10ceee47ea704498f422a3
Ishan Gulhane <ishan.gulhane@ibm.com>
2020-04-10 11:20:24 -0700

    fix coverage

--- test/AppIdTest.js
+++ test/AppIdTest.js
@@ -68,6 +68,23 @@ describe('AppID tests', () => {
 			assert.equal(res.accessTokenPayload, 'tokenPayload');
 		});
 
+		it('should return tokens with no origin ', async () => {
+			const appID = new AppID({
+				popup: new PopupControllerMock({invalidState: false, error: false, invalidOrigin: false}),
+				iframe: new IFrameControllerMock({invalidState: false, error: false, invalidOrigin: false}),
+				openIdConfigResource: new OpenIdConfigurationResourceMock(),
+				utils: new Utils(),
+				requestHandler: new RequestHandlerMock(),
+				w: {location : {protocol: 'http', hostname: 'localhost', port: '3005'}},
+				url: URL
+			});
+			await appID.init(defaultInit);
+			let res = await appID.signin();
+			assert.equal(res.accessToken, 'accessToken');
+			assert.equal(res.idToken, 'idToken');
+			assert.equal(res.accessTokenPayload, 'tokenPayload');
+		});
+
 		it('should return error - invalid state', async () => {
 			const appID = new AppID({
 				popup: new PopupControllerMock({invalidState: true, error: false}),
